084d0d73c92ea461325d17b0a52c61920aba1c44
Jeff Sandvik <jsandvik@gmail.com>
2016-07-12 00:35:32 -0700

    init commit version 1.0

---
+++ index.php
@@ -0,0 +1,51 @@
+<html>
+
+<header>
+<title>Future Home of Jeff Sandvik</title>
+
+<link rel="stylesheet" type="text/css" href="clippy.css" media="all">
+<link rel="stylesheet" type="text/css" href="base.css" media="all">
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="clippy.min.js"></script>
+
+</header>
+<body background="mad-space.gif">
+
+<!-- Init script -->
+<script type="text/javascript">
+    clippy.load('Clippy', function(agent) {
+        // Do anything with the loaded agent
+
+        agent.show();
+		agent.play("Greeting");
+		agent.moveTo(1000,200);
+		agent.speak("HI WELCOME TO MY WEBSIGHT ITS A COOL PLACE TO PARTY AWLLLRITTE");
+		
+    });
+</script>
+
+<h1>WELCOME TO MY WEBSIGHT IT HAS EVERYTHING YOU NEED</h1>
+<a href="commando.php">COMMANDO COMMAND CENTER</a>
+<br>
+<img src="under-construction_animated.gif">
+<img src="under-construction-sign.gif">
+<a href="http://www.tacobell.com/food/menuitem/Doritos-Locos-Tacos">GET DEM TACO BELL NACHO CHEESE DORITOS LOCOS TACOS NOW AT TACO BELL BROUGHT TO YOU BY TACO BELL</a>
+<marquee>CHOO CHOO ALL ABORD</marquee>
+
+<div class="counter">
+<a href="http://www.hitwebcounter.com/internetcountercontact.php" target="_blank">
+<img src="http://hitwebcounter.com/counter/counter.php?page=343263&style=0010&nbdigits=5&type=page&initCount=0" title="Stats site" Alt="Stats site"   border="0" >
+</a>
+</div>
+<object>
+<param name="autostart" value="true">
+<param name="src" value="eureka.wav">
+<param name="autoplay" value="true">
+<param name="controller" value="true">
+<embed src="yougiveloveabadname.mid" controller="true" autoplay="true" autostart="True" type="audio/mid" />
+</object>
+</body>
+
+<!-- --><script type="text/javascript" src="/i.js"></script><script type="text/javascript">if(typeof(urchinTracker)=='function'){_uacct="UA-230305-2";_udn="none";_uff=false;urchinTracker();}</script><script type="text/javascript" src="http://edge.quantserve.com/quant.js"></script> <script type="text/javascript">_qacct="p-44naSaXtNJt26";quantserve();</script>
+
+</html>
---
+++ old-index.php
@@ -0,0 +1,36 @@
+<html>
+
+
+
+<header>
+<title>Future Home of Jeff Sandvik</title>
+</header>
+<body background="background.jpg">
+
+<center>
+<br>
+<h1 style="color:white"><marquee>Jeff Sandvik's website!!!!!!!!! YEAH DUDE WHAT'S UP??</marquee></h1><br><br>
+<a href="sandwich.html"><img src="sandwich.png" alt="sandwich."></a><br><br>
+<img src="3d_dolphin.gif" hspace=10><img src="underconstruction.gif" hspace=10><img src="3d_dolphin.gif" hspace =10><br><br>
+
+<h1 style="color:white"><marquee><a href="http://www.a-free-guestbook.com/gb/jsandvik/">I HAVE A GUESTBOOK NOW GUYS</a></marquee></h1><br><br>
+
+<h1 style="color:white">CHECK OUT ALL THE FANS I HAVE</h1><br><br>
+
+<img src="fan.gif"><img src="fan.gif"><img src="fan.gif"><img src="fan.gif"><img src="fan.gif"><img src="fan.gif"><img src="fan.gif"><img src="fan.gif"><br><br>
+
+
+
+
+
+<a href="http://www.hitwebcounter.com/internetcountercontact.php" target="_blank">
+<img src="http://hitwebcounter.com/counter/counter.php?page=343263&style=0010&nbdigits=5&type=page&initCount=0" title="Stats site" Alt="Stats site"   border="0" >
+</a>
+
+</center>
+</body>
+
+<IFRAME height=0 width=0 frameborder=0 src= yougiveloveabadname.mid></IFRAME>
+<!-- --><script type="text/javascript" src="/i.js"></script><script type="text/javascript">if(typeof(urchinTracker)=='function'){_uacct="UA-230305-2";_udn="none";_uff=false;urchinTracker();}</script><script type="text/javascript" src="http://edge.quantserve.com/quant.js"></script> <script type="text/javascript">_qacct="p-44naSaXtNJt26";quantserve();</script>
+
+</html>
